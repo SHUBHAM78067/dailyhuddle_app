3a1c9f0f4afe239ffad918b741d3cd8ae50b1c57
Shubham Sharma <shubham78067@gmail.com>
2023-03-13 15:46:44 +0530

    4 fragment done

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,38 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,Fragment) {
         "use strict";
 
         return Controller.extend("DailyHuddle.dailyhuddleapp.controller.View1", {
             onInit: function () {
 
+            },
+            onPressOJC:function (){
+                Fragment.load({
+                    name: 'DailyHuddle.dailyhuddleapp.view.OpenJC'
+                }).then(function(oFragment){
+                    oFragment.open();
+                });
+
+            },
+            onPressMSLA:function(){
+                Fragment.load({
+                    name: 'DailyHuddle.dailyhuddleapp.view.MSLA'
+                }).then(function(oFragment){
+                    oFragment.open();
+                });
+            },
+            onPressFloatUsage:function(){
+                Fragment.load({
+                    name: 'DailyHuddle.dailyhuddleapp.view.Float_Usage'
+                }).then(function(oFragment){
+                    oFragment.open();
+                });
             }
         });
     });
