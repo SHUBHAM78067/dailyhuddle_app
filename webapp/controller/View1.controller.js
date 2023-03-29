e5c9037763306f638e2aa3aef76497e42582a69c
Shubham Sharma <shubham78067@gmail.com>
2023-03-29 16:43:29 +0530

    ok

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,38 +1,60 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/core/Fragment"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller,Fragment) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/Fragment"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, Fragment) {
+    "use strict";
 
-        return Controller.extend("DailyHuddle.dailyhuddleapp.controller.View1", {
-            onInit: function () {
-
-            },
-            onPressOJC:function (){
-                // Fragment.load({
-                //     name: 'DailyHuddle.dailyhuddleapp.view.OpenJC'
-                // }).then(function(oFragment){
-                //     oFragment.open();
-                // });
-
-            },
-            onPressMSLA:function(){
-                // Fragment.load({
-                //     name: 'DailyHuddle.dailyhuddleapp.view.MSLA'
-                // }).then(function(oFragment){
-                //     oFragment.open();
-                // });
-            },
-            onPressFloatUsage:function(){
-                // Fragment.load({
-                //     name: 'DailyHuddle.dailyhuddleapp.view.Float_Usage'
-                // }).then(function(oFragment){
-                //     oFragment.open();
-                // });
-            }
-        });
+    return Controller.extend("DailyHuddle.dailyhuddleapp.controller.View1", {
+      onInit: function () {},
+      onPressOJC: function () {
+        // Fragment.load({
+        //     name: 'DailyHuddle.dailyhuddleapp.view.OpenJC'
+        // }).then(function(oFragment){
+        //     oFragment.open();
+        // });
+      },
+      oPopupSupplier: null,
+      onPressMSLA: function (oEvent) {
+        debugger;
+        // alert("thuis functionality is under construction");
+        var that = this;
+        if (!this.oPopupSupplier) {
+          Fragment.load({
+            name: "DailyHuddle.dailyhuddleapp.view.popup",
+            type: "XML",
+          }).then(function (oFragment) {
+            // inside promise and call back function , we cannot access this pointer
+            // this is controller object, so we need to create a local variable for controller 
+            // object outside the promise and call back 
+            that.oPopupSupplier = oFragment;
+            // check sdk methos for select dialog or for which you want to open 
+            that.oPopupSupplier.open();
+          });
+        } else {
+          this.oPopupSupplier.open();
+        }
+      },
+      onPressFloatUsage: function () {
+        var that = this;
+        if (!this.oPopupSupplier) {
+          Fragment.load({
+            name: "DailyHuddle.dailyhuddleapp.view.popup",
+            type: "XML",
+          }).then(function (oFragment) {
+            // inside promise and call back function , we cannot access this pointer
+            // this is controller object, so we need to create a local variable for controller 
+            // object outside the promise and call back var that= this ;
+            that.oPopupSupplier = oFragment;
+            // check sdk methos for select dialog or for which you want to open 
+            that.oPopupSupplier.open();
+          });
+        } else {
+          this.oPopupSupplier.open();
+        }
+ 
+      }
     });
+  }
+);
