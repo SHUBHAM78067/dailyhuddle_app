341c01a7296350a0bb5b3fa7ef9a9ef1ed4cbcdc
Shubham Sharma <shubham78067@gmail.com>
2023-03-30 14:54:55 +0530

    ui structure done

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,53 +7,111 @@ sap.ui.define(
     "use strict";
 
     return Controller.extend("DailyHuddle.dailyhuddleapp.controller.View1", {
-      onInit: function () {},
-      onPressOJC: function () {
-        // Fragment.load({
-        //     name: 'DailyHuddle.dailyhuddleapp.view.OpenJC'
-        // }).then(function(oFragment){
-        //     oFragment.open();
-        // });
+      onInit: function () {
+
+
       },
-      oPopupSupplier: null,
+      onPressOJC: function () {
+        this.getView().byId("OpenjcToolBar").setVisible(true);
+        this.getView().byId("uiTab1").setVisible(true);
+        this.getView().byId("idMSLAToolbar").setVisible(false);
+        this.getView().byId("uiTab2").setVisible(false);
+        this.getView().byId("idFloatTable").setVisible(false);
+    
+      },
       onPressMSLA: function (oEvent) {
-        debugger;
-        // alert("thuis functionality is under construction");
-        var that = this;
-        if (!this.oPopupSupplier) {
-          Fragment.load({
-            name: "DailyHuddle.dailyhuddleapp.view.popup",
-            type: "XML",
-          }).then(function (oFragment) {
-            // inside promise and call back function , we cannot access this pointer
-            // this is controller object, so we need to create a local variable for controller 
-            // object outside the promise and call back 
-            that.oPopupSupplier = oFragment;
-            // check sdk methos for select dialog or for which you want to open 
-            that.oPopupSupplier.open();
-          });
-        } else {
-          this.oPopupSupplier.open();
-        }
+       this.getView().byId("idMSLAToolbar").setVisible(true);
+       this.getView().byId("uiTab2").setVisible(true);
+       this.getView().byId("OpenjcToolBar").setVisible(false);
+       this.getView().byId("uiTab1").setVisible(false);
+       this.getView().byId("idFloatTable").setVisible(false);
+       
       },
       onPressFloatUsage: function () {
-        var that = this;
-        if (!this.oPopupSupplier) {
-          Fragment.load({
-            name: "DailyHuddle.dailyhuddleapp.view.popup",
-            type: "XML",
-          }).then(function (oFragment) {
-            // inside promise and call back function , we cannot access this pointer
-            // this is controller object, so we need to create a local variable for controller 
-            // object outside the promise and call back var that= this ;
-            that.oPopupSupplier = oFragment;
-            // check sdk methos for select dialog or for which you want to open 
-            that.oPopupSupplier.open();
-          });
-        } else {
-          this.oPopupSupplier.open();
-        }
- 
+        this.getView().byId("idMSLAToolbar").setVisible(false);
+        this.getView().byId("uiTab2").setVisible(false);
+        this.getView().byId("OpenjcToolBar").setVisible(false);
+        this.getView().byId("uiTab1").setVisible(false);
+        this.getView().byId("idFloatTable").setVisible(true);
+        var oTableData = [{
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }, {
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }, {
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }, {
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }, {
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }, {
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }, {
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }, {
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }, {
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }, {
+          "JcNo": "",
+          "FloatPartNo": "",
+          "FloatSerialNo": ""
+        }];
+        var oTableModel = new sap.ui.model.json.JSONModel(oTableData);
+        this.getView().byId("idFloatTable").setModel(oTableModel, "oFloatUsageModel");
+      },
+      onPressOEOS:function(){
+        this.getView().byId("oeostoolbarid").setVisible(true);
+        this.getView().byId("uiTab3").setVisible(true);
+        this.getView().byId("toolbarrmsla").setVisible(false);
+        this.getView().byId("uiTab4").setVisible(false);
+        this.getView().byId("toolbarrsmsla").setVisible(false);
+        this.getView().byId("uiTab5").setVisible(false);
+      },
+      onPressRCMSLA:function(){
+        this.getView().byId("oeostoolbarid").setVisible(false);
+        this.getView().byId("uiTab3").setVisible(false);
+        this.getView().byId("toolbarrmsla").setVisible(true);
+        this.getView().byId("uiTab4").setVisible(true);
+        this.getView().byId("toolbarrsmsla").setVisible(false);
+        this.getView().byId("uiTab5").setVisible(false);
+      },
+      onPressRSMSLA:function(){
+        this.getView().byId("oeostoolbarid").setVisible(false);
+        this.getView().byId("uiTab3").setVisible(false);
+        this.getView().byId("toolbarrmsla").setVisible(false);
+        this.getView().byId("uiTab4").setVisible(false);
+        this.getView().byId("toolbarrsmsla").setVisible(true);
+        this.getView().byId("uiTab5").setVisible(true);
+      },
+      onPressOC:function(){
+        this.getView().byId("toolbaroc").setVisible(true);
+        this.getView().byId("uiTab6").setVisible(true);
+        this.getView().byId("toolbarmslac").setVisible(false);
+        this.getView().byId("uiTab7").setVisible(false);
+      },
+      onPressMSLAC:function(){
+        this.getView().byId("toolbaroc").setVisible(false);
+        this.getView().byId("uiTab6").setVisible(false);
+        this.getView().byId("toolbarmslac").setVisible(true);
+        this.getView().byId("uiTab7").setVisible(true);
       }
     });
   }
